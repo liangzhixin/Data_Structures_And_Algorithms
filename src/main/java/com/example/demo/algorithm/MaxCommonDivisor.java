adc97875830f9e3049411abba6e86695407ec688
lzx <zhixin.liang@jieshunpay.cn>
2019-12-25 11:10:58 +0800

    初始化项目

---
+++ src/main/java/com/example/demo/algorithm/MaxCommonDivisor.java
@@ -0,0 +1,11 @@
+package com.example.demo.algorithm;
+
+/**
+ * @ClassName com.example.demo.algorithm.MaxCommonDivisor
+ * @Description 求最大公约数
+ * @Author lzx
+ * @Date 2019-12-25 10:56
+ * @Version 1.0
+ */
+public class MaxCommonDivisor {
+}
