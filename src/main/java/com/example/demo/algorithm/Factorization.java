e626801fb0ce149ed938f8998d187d3d2c93f7d6
lzx <zhixin.liang@jieshunpay.cn>
2019-12-25 17:23:09 +0800

    产生一个不大于给定整数n的连续质数序列

---
+++ src/main/java/com/example/demo/algorithm/Factorization.java
@@ -0,0 +1,48 @@
+package com.example.demo.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName com.example.demo.algorithm.Factorization
+ * @Description 产生一个不大于给定整数n的连续质数序列
+ * @Author lzx
+ * @Date 2019-12-25 11:41
+ * @Version 1.0
+ */
+@Slf4j
+public class Factorization {
+
+    public static void main(String[] args) {
+        Factorization test = new Factorization();
+        System.out.println(Arrays.toString(test.methodOne(25)));
+    }
+
+    /**
+     * 埃拉托色尼筛选法
+     */
+    private int[] methodOne(int n) {
+        int[] array = new int[n + 1];
+        for (int i = 2; i < array.length; i++) {
+            array[i] = 1;
+        }
+        int length = array.length - 2;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] == 1 && j % i == 0) {
+                    array[j] = 0;
+                    length--;
+                }
+            }
+        }
+        int[] res = new int[length];
+        int num = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                res[num++] = i;
+            }
+        }
+        return res;
+    }
+}
--- src/main/java/com/example/demo/algorithm/MaxCommonDivisor.java
+++ src/main/java/com/example/demo/algorithm/MaxCommonDivisor.java
@@ -2,10 +2,35 @@ package com.example.demo.algorithm;
 
 /**
  * @ClassName com.example.demo.algorithm.MaxCommonDivisor
- * @Description 求最大公约数
+ * @Description 求两个数的最大公约数
  * @Author lzx
  * @Date 2019-12-25 10:56
  * @Version 1.0
  */
 public class MaxCommonDivisor {
+
+    public static void main(String[] args) {
+//        MaxCommonDivisor test = new MaxCommonDivisor();
+//        System.out.println(test.methodOne(60,24));
+    }
+
+    /**
+     * 方法一：欧几里得算法
+     */
+    private int methodOne(int a, int b) {
+        int m = a, n = b, r;
+        while((r = m % n) != 0){
+            m = n;
+            n = r;
+        }
+        return n;
+    }
+
+    /**
+     * 方法二：质因数分解法
+     */
+    private int methodTwo(int a, int b){
+
+        return 0;
+    }
 }
